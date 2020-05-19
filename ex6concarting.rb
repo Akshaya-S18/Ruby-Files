f7ebab8ccb3c58425ab373b65873abe4b8c34b97
Akshaya-S18 <65398963+Akshaya-S18@users.noreply.github.com>
2020-05-19 15:25:21 +0530

    concatenating two strings and checking boolean values

---
+++ ex6concarting.rb
@@ -0,0 +1,23 @@
+# concardinating..
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary ='binary'
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts " I also said: '#{y}'."
+
+hiraloius= false
+joke_evaluation= "Isn't that joke so funy?!#{hiraloius}"
+
+puts joke_evaluation
+
+w = "This is left of ..."
+e = "a string with right side."
+
+puts w + e
+
