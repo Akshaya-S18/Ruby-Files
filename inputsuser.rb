b7266b6e3e12cab390a024ad06f087f285c0cfde
Akshaya-S18 <65398963+Akshaya-S18@users.noreply.github.com>
2020-05-21 00:43:58 +0530

    get user inputs arithmetic cal

---
+++ inputsuser.rb
@@ -0,0 +1,30 @@
+#gets input from user
+
+ # puts "\tWhat is your name?"
+# a = gets.chomp
+# puts "\tyour name is #{a}"
+
+#used to get string as o/p class..
+# a = gets.chomp
+# puts "#{a}"
+# puts "#{a.class}"
+
+#used to get integer as o/p class..
+# a = gets.chomp.to_i
+#  puts "#{a}"
+# puts "#{a.class}"
+
+##used to get integer as o/p class..
+# puts "enter a:" 
+# a = gets.chomp
+# puts "enter b:"
+
+# b = gets.chomp
+# puts "there is  #{a} and #{b} . the sum  of these are #{a+b} " # its not adding its just concardinatin displaying
+
+#used to get integer from user and adding that..
+ puts "enter a:" 
+a = gets.chomp.to_i
+ puts "enter b:" 
+b = gets.chomp.to_i
+puts "You have entered #{a} and #{b} and their sum is #{a+b}" #its adding the int resuls & displaying results
