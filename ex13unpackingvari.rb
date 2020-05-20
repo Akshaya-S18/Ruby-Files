e8e1c682702451e32ec1529c6fcf0f124d2aa2d0
Akshaya-S18 <65398963+Akshaya-S18@users.noreply.github.com>
2020-05-21 00:49:14 +0530

    unpacking variables by arguments argv

---
+++ ex13unpackingvari.rb
@@ -0,0 +1,9 @@
+#unpacking variables
+puts "Unpacking variables Arguments"
+puts "."*29
+first, second, third = ARGV
+
+# a = gets.chomp
+puts "Your first variable is: #{ first}"
+puts "Your second variable is: #{ second}"
+puts "Your third variable is: #{ third}"
