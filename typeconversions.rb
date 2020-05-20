ceff28af405c13d1eac8190f36f2ab02617cab98
Akshaya-S18 <65398963+Akshaya-S18@users.noreply.github.com>
2020-05-21 00:59:21 +0530

    Type conversions of variables

---
+++ typeconversions.rb
@@ -0,0 +1,13 @@
+#Type conversions
+puts "Type conversions"
+puts "."*16
+a=2.56
+b = a.to_i
+c = 34
+d = c.to_f
+
+# As fr eg we have seen, a.to_i converted 2.34 to an integer 2, and c.to_f converted 34 to a float 34.0.
+
+puts "a = #{a}\nb = #{b}\nc = #{d}"
+puts "a is #{a.class}\nb is #{b.class}\nc is #{d.class}"
+
