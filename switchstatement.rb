56300942a842f279fc70393369c6f1bc0090b2df
Akshaya-S18 <65398963+Akshaya-S18@users.noreply.github.com>
2020-06-18 20:21:13 +0530

    SwitchStatement

---
+++ switchstatement.rb
@@ -0,0 +1,18 @@
+# switch  statement...
+
+print "Enter your day: "   
+day = gets.chomp   
+case day   
+when "Tuesday"   
+  puts 'Wear Red or Orange'   
+when "Wednesday"   
+  puts 'Wear Green'   
+when "Thursday"   
+  puts 'Wear Yellow'   
+ when "Friday"   
+  puts 'Wear White'   
+ when "Saturday"   
+  puts 'Wear Black'   
+else   
+  puts "Wear Any color & have a good day.."   
+end   
