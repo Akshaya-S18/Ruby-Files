7c95d219076c895c6cbbd6d94114cd759fbd0ce5
Akshaya-S18 <65398963+Akshaya-S18@users.noreply.github.com>
2020-06-18 20:22:31 +0530

    While Loop

---
+++ whileloop.rb
@@ -0,0 +1,16 @@
+# while loop..
+puts "enter no:"
+i = gets.chomp.to_i
+while i<=10
+	puts i*14
+	i=i+1
+end
+
+# again while loop.
+puts "enter no"
+x = gets.chomp.to_i   
+while x >= 0    
+  puts x   
+  # x -=1   
+  x= x-1
+end   
