16e6b5dde2740f14df8130672ee338ac43f10ee6
Akshaya-S18 <65398963+Akshaya-S18@users.noreply.github.com>
2020-06-18 20:29:26 +0530

    Digital Dice doing

---
+++ digitaldice.rb
@@ -0,0 +1,12 @@
+# digital dice
+puts "enter the lower limit of dice"
+a= gets.chomp.to_i
+puts " enter the higher limit of dice"
+b= gets.chomp.to_i
+puts "type q to Quit or any other key/enter to continue"
+while true
+	puts ">>> #{rand(a..b)}"#rand is generating random number between a and b
+	if gets.chomp== 'q' # #if 'q' is entered then come out of loop
+		break;
+	end
+end
