eaf0dfd1ca6b81d051ac57348c66441881e397fe
Akshaya-S18 <65398963+Akshaya-S18@users.noreply.github.com>
2020-06-18 20:30:37 +0530

    DoWhile

---
+++ dowhile.rb
@@ -0,0 +1,9 @@
+# do while..
+loop do   
+  puts "Checking for answer"   
+  answer = gets.chomp   
+  if answer != '5'   #if INPUT IS 5 MEANS IT WILL CONTINUE ELSE IT BREAK
+
+    break   
+  end   
+end  
